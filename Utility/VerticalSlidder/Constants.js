4e372b36414b17d5170d81695cf9da983a02dfe7
kanha <kanha.agr11@gmail.com>
2022-03-02 23:00:37 +0530

    deleted everything

--- Utility/VerticalSlidder/Constants.js
+++
@@ -1,7 +0,0 @@
-import { Dimensions, PixelRatio } from 'react-native';
-const { height } = Dimensions.get('screen');
-export const DrawerState = {
-  // Open: height - 230,
-  Peek: 680,
-  Closed: 0,
-}
